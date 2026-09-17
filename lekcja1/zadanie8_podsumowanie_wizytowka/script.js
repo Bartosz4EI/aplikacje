13f3716b85fca484b925472eb7a19d20198fba0a
Bartosz 4EI <u8806010587@gmail.com>
2026-09-17 09:07:21 +0200

    Add files via upload

---
+++ lekcja1/zadanie8_podsumowanie_wizytowka/script.js
@@ -0,0 +1,6 @@
+// Twoje rozwiazanie
+// Bartosz
+const imie = prompt('Podaj Imie');
+const klasa = prompt('Podaj Klase');
+document.write(imie + "<br>" + klasa + "<br>" + "to moja wizytówka");
+console.log(imie + ' ' + klasa);
