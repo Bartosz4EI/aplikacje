065d8864d1cadb55916f89d24310667efbf331d0
Bartosz 4EI <u8806010587@gmail.com>
2026-09-24 09:06:21 +0200

    Format VAT amount to two decimal places
    Updated VAT calculation to format VAT amount to two decimal places.

--- lekcja2/zadanie6_faktura_vat/script.js
+++ lekcja2/zadanie6_faktura_vat/script.js
@@ -3,4 +3,4 @@ const STAWKA_VAT = 0.23;
 nazwa = prompt("Podaj nazwe towaru");
 cena_n = Number(prompt("Podaj cenę netto"));
 cena_b = cena_n + cena_n*STAWKA_VAT;
-document.write(nazwa + ", netto " + cena_n + " PLN, VAT " + cena_n*STAWKA_VAT + " i brutto " + cena_b + " PLN" )
+document.write(nazwa + ", netto " + cena_n + " PLN, VAT " + (cena_n*STAWKA_VAT).toFixed(2) + " i brutto " + cena_b + " PLN" )
